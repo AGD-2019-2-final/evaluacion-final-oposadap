1a8d6e06654b043f3949e91403ce6feb0a3129d3
Orlando Antonio Posada Prieto <50972514+oposadap@users.noreply.github.com>
2020-02-14 21:04:32 -0500

    correccion

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -2,4 +2,23 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
 
+    curkey = None
+    total= None
+    
+    for line in sys.stdin:
+        key, valor_1 = line.split("\t")
+        valor_1 = int(valor_1)
+        
+        if key == curkey:
+            total += valor_1        
+        else:
+            if curkey is not None: 
+                sys.stdout.write("{},{}\n".format(curkey, total))
+                
+            curkey = key
+            total = valor_1
+            
+    sys.stdout.write("{},{}\n".format(curkey, total))
+                
